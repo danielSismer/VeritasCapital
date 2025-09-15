f7a18598d28d9ac350cd5c1e2b7eae725f415d5a
Hugo <hugo_paim@estudante.sesisenai.org.br>
2025-09-14 21:27:08 -0300

    chore: adiciona docker-compose e script sql para configuração inicial

---
+++ src/com/banco/resources/veritas_capital.sql
@@ -0,0 +1,38 @@
+create database veritas_capital;
+
+use veritas_capital;
+
+create table titular(
+id int primary key auto_increment,
+nome varchar(45) not null,
+cpf varchar(15) not null
+);
+
+create table conta(
+id int primary key auto_increment,
+titular int not null,
+foreign key (titular) references titular(id),
+numero varchar(60) unique not null,
+saldo double(8,2) not null,
+tipo enum("CONTA_CORRENTE", "CONTA_POUPANÇA") not null
+);
+
+create table conta_poupanca(
+id int primary key auto_increment,
+taxaRendimento double (2,2) not null
+);
+
+create table conta_corrente(
+id int primary key auto_increment,
+movimentacaoDiaria double (8,2) not null
+);
+
+create table movimentacao(
+id int primary key auto_increment,
+titular_remetente int not null,
+foreign key (titular_remetente) references titular(id),
+titular_destinatario int not null,
+foreign key (titular_destinatario) references titular(id),
+tipo enum("DEPOSITO", "SAQUE", "TRANSFERENCIA")	not null,
+data_horario datetime not null default current_timestamp 
+);
