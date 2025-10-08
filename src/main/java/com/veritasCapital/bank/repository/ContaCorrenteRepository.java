cadd18ba02df91455c1f6f3b0c555c869eb3c3d5
Hugo Deleon <hugo_paim@estudante.sesisenai.org.br>
2025-10-08 16:20:09 -0300

    BANK-32 feat: adiciona estrutura de repository com save e list (#5)
    Adicionar commits da branch master, criada por engano, para a feat/spring, principal para desenvolvimento Spring Boot.

--- src/main/java/com/veritasCapital/bank/repository/ContaCorrenteRepository.java
+++ src/main/java/com/veritasCapital/bank/repository/ContaCorrenteRepository.java
@@ -1,7 +1,39 @@
 package com.veritasCapital.bank.repository;
 
 import com.veritasCapital.bank.model.ContaCorrente;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface ContaCorrenteRepository extends JpaRepository<ContaCorrente, Integer> {
+@Repository
+public class ContaCorrenteRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ContaCorrenteRepository (JdbcTemplate jT){
+        this.jdbcTemplate = jT;
+    }
+
+    public void salvar (ContaCorrente cc ){
+            String sql = """
+                    INSERT INTO conta_corrente (id, movimentacaoDiaria)
+                    VALUES (?, ?);
+                    """;
+            jdbcTemplate.update(sql, cc.getId(), cc.getMovimentacaoDiaria());
+        System.out.println("Conta Corrente registrada com Sucesso!!!");
+    }
+
+    public List<ContaCorrente> listar (){
+        String query = """
+                SELECT id, movimentacaoDiaria
+                FROM conta_corrente
+                """;
+        return jdbcTemplate.query(query, (rs, numRow) -> {
+            var c = new ContaCorrente();
+            c.setId(rs.getInt("id"));
+            c.setMovimentacaoDiaria(rs.getDouble("movimentacaoDiaria"));
+            return c;
+        });
+
+    }
 }
--- src/main/java/com/veritasCapital/bank/repository/ContaPoupancaRepository.java
+++ src/main/java/com/veritasCapital/bank/repository/ContaPoupancaRepository.java
@@ -1,7 +1,39 @@
 package com.veritasCapital.bank.repository;
 
 import com.veritasCapital.bank.model.ContaPoupanca;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface ContaPoupancaRepository extends JpaRepository<ContaPoupanca, Integer> {
+@Repository
+public class ContaPoupancaRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ContaPoupancaRepository (JdbcTemplate jT ){
+        this.jdbcTemplate = jT;
+    }
+
+    public void salvar (ContaPoupanca contaPoupanca ){
+        String query = """
+                INSERT INTO conta_poupanca 
+                (id, taxaRendimento)
+                VALUES 
+                (?, ?);
+                """;
+        jdbcTemplate.update(query, contaPoupanca.getId(), contaPoupanca.getTaxaRendimento());
+    }
+
+    public List<ContaPoupanca> select (){
+        String query = """
+                SELECT id, taxaRendimento
+                FROM conta_poupanca
+                """;
+        return jdbcTemplate.query(query, (rs, numRow) -> {
+            var contaPoupanca = new ContaPoupanca();
+            contaPoupanca.setId(rs.getInt("id"));
+            contaPoupanca.setTaxaRendimento(rs.getDouble("taxaRendimento"));
+            return contaPoupanca;
+        });
+    }
 }
